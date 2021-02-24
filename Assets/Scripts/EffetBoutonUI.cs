17fb3db76082a8099be66c333306e158d6b1c83b
SGLover28 <lilian.hennechart.ETU@univ-lemans.fr>
2021-02-24 18:03:09 +0100

    Ajout UI, mouvement et collision
    On peut maintenant bouger ! En plus, il y a un template pour les explication et les murs nous empêchent de bouger (pour certians)

---
+++ Assets/Scripts/EffetBoutonUI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EffetBoutonUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public UnityEngine.UI.Button UiButton;
+    public string infos;
+
+    private bool estActif = false;
+
+   
+    
+    void Start()
+    {
+        Text t = GameObject.Find("Information").GetComponent<UnityEngine.UI.Text>(); 
+        UiButton.onClick.AddListener(()=>{
+            if(!estActif){
+            estActif =true;
+            t.text = infos;}
+        else{
+            t.text = "";
+            estActif = false;
+        }
+    });
+        
+    }
+    
+   
+   
+}
